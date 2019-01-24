066d884d3f3361da355aebb3d057a656f92f0169
theo <tkleinen@gmail.com>
2019-01-24 09:44:26 +0100

    homepage has links to dashboards

--- noorderhaaks/views.py
+++ noorderhaaks/views.py
@@ -5,10 +5,30 @@ Created on Jan 2, 2019
 '''
 from acacia.data.views import ProjectDetailView
 from acacia.data.models import Project
+import json
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.views.generic.detail import DetailView
 
-class HomeView(ProjectDetailView):
+class HomeView(DetailView):
+    template_name = 'home.html'
+    
     def get_context_data(self, **kwargs):
-        context = ProjectDetailView.get_context_data(self, **kwargs)
+        context = DetailView.get_context_data(self, **kwargs)
+        project = self.get_object()
+        content = []
+        for ploc in project.projectlocatie_set.all():
+            for loc in ploc.meetlocatie_set.all():
+                pos = loc.latlon()
+                content.append({
+                                'id': loc.id,
+                                'name': loc.name,
+                                'lat': pos.y,
+                                'lon': pos.x,
+                                'info': render_to_string('data/meetlocatie_info.html', {'object': loc})
+                                })
+        context['content'] = json.dumps(content)
+        context['apikey'] = settings.GOOGLE_MAPS_API_KEY
         context['maptype'] = 'SATELLITE'
         context['zoom'] = 14
         return context
